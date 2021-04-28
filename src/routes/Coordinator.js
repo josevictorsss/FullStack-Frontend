1e95676eb2e4c81b394bc2c223b9a2a1c7e6f4b3
José Victor T. Ferreira <josevictortf@gmail.com>
2021-04-28 14:21:44 -0300

    feat: implement coordinator

---
+++ src/routes/Coordinator.js
@@ -0,0 +1,11 @@
+export const goToMusics = (history) => {
+  history.push("/musics");
+};
+
+export const goToLogin = (history) => {
+  history.push("/");
+};
+
+export const goToSignup = (history) => {
+  history.push("/signup");
+};
