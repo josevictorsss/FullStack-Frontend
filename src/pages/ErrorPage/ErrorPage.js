d50f713b85efb1384dfb512f707725101a6fd1b5
José Victor T. Ferreira <josevictortf@gmail.com>
2021-05-11 10:34:18 -0300

    feat: implement error button in error page

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { useHistory } from "react-router";
 import logo from "../../assets/logo.png";
 import Header from "../../components/Header/Header";
-import { Container, LogoImg } from "./styled";
+import { goToLogin } from "../../routes/Coordinator";
+import { Container, LogoImg, ErrorButton } from "./styled";
 
 const ErrorPage = () => {
   document.title = "Error";
+  const history = useHistory();
   return (
     <>
       <Header />
       <Container>
-        <LogoImg src={logo} />
+        <LogoImg src={logo} alt="Error" />
         <h1>Página não encontrada :(</h1>
+        <ErrorButton onClick={() => goToLogin(history)}>
+          {" "}
+          VOLTE A PÁGINA INICIAL
+        </ErrorButton>
       </Container>
     </>
   );
--- src/pages/ErrorPage/styled.js
+++ src/pages/ErrorPage/styled.js
@@ -16,3 +16,20 @@ export const LogoImg = styled.img`
   width: 50%;
   margin: 68px 128px 16px;
 `;
+
+export const ErrorButton = styled.button`
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 20px;
+  font-size: 16px;
+  font-weight: bold;
+  transition: 0.5s;
+  background-color: royalblue;
+  color: blanchedalmond;
+  margin-top: 50px;
+  :hover {
+    background-color: lightslategrey;
+    margin-top: -8px;
+  }
+`;
